99ad1e5df3857a58c3a180faf86dad3baf18a24f
Rizky Khapidsyah <rizkykhapidsyah@gmail.com>
2020-11-19 09:09:40 +0700

    Initial commit

---
+++ database1i/src/com/wilis/database1/database1.java
@@ -0,0 +1,45 @@
+package com.wilis.database1;
+
+import android.app.Activity;
+import android.database.Cursor;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class database1 extends Activity {
+    /** Called when the activity is first created. */
+	@Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+
+        DBAdapter db = new DBAdapter(this);
+
+        db.open();
+        Cursor c = db.getAllTitles();
+        if (c.moveToFirst())
+        {
+            do {          
+                DisplayTitle(c);
+            } while (c.moveToNext());
+        }
+        db.close();
+
+        
+    }
+	
+    public void DisplayTitle(Cursor c)
+    {
+        Toast.makeText(this, 
+                "id: " + c.getString(0) + "\n" +
+                "ISBN: " + c.getString(1) + "\n" +
+                "TITLE: " + c.getString(2) + "\n" +
+                "PUBLISHER:  " + c.getString(3),
+                Toast.LENGTH_LONG).show();        
+    } 
+
+
+}
+
+
+
+
